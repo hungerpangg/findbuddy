74973d3101a492405dbd3e907df4393199299e00
Ka Wai <pangkawai22@gmail.com>
2023-10-20 02:45:30 +0800

    Update CSP headers, icon

--- findbuddy_ui/src/App.js
+++ findbuddy_ui/src/App.js
@@ -13,6 +13,8 @@ import Chat from "./components/Chat";
 import FrontPage from "./components/FrontPage";
 import PrivateRoute from "./components/PrivateRoute";
 import NotFound from "./components/NotFound";
+import { HelmetProvider, Helmet } from "react-helmet-async";
+const helmetContext = {};
 
 function App() {
 	const {
@@ -20,66 +22,87 @@ function App() {
 	} = useContext(AuthenticateContext);
 
 	return (
-		<div style={{ margin: "0", padding: "0" }}>
-			<Router>
-				{isAuthenticated ? <SignedInNavbar /> : <SignedOutNavbar />}
-				<Routes>
-					<Route
-						path="/"
-						element={
-							<PrivateRoute
-								element={<Home />}
-								alternateElement={<FrontPage />}
-								isAuthenticated={isAuthenticated}
-							/>
-						}
+		<HelmetProvider context={helmetContext}>
+			<div style={{ margin: "0", padding: "0" }}>
+				<Helmet>
+					<title>FindBuddy: Find buddies with similar interests as you</title>
+					<meta
+						name="description"
+						content="Find buddies with similar interests as you to do the
+						things you love together!"
 					/>
-					<Route
-						path="/home"
-						element={
-							<PrivateRoute
-								element={<Home />}
-								alternateElement={<FrontPage />}
-								isAuthenticated={isAuthenticated}
-							/>
-						}
+					<meta
+						httpEquiv="Content-Security-Policy"
+						content={`
+                      default-src 'self';
+                      script-src 'self' http://localhost:4000/;
+					  connect-src 'self' http://localhost:4000/ wss://api.chatengine.io/ https://api.chatengine.io/;
+                      img-src 'self' https://findbuddy-pictures.s3.ap-southeast-1.amazonaws.com/ https://chat-engine-assets.s3.amazonaws.com/;
+					  style-src 'self' 'unsafe-inline';
+						
+                `}
 					/>
-					<Route
-						path="/signin"
-						element={
-							<PrivateRoute
-								element={<Home />}
-								alternateElement={<Signin />}
-								isAuthenticated={isAuthenticated}
-							/>
-						}
-					/>
-					<Route
-						path="/signup"
-						element={
-							<PrivateRoute
-								element={<Home />}
-								alternateElement={<Signup />}
-								isAuthenticated={isAuthenticated}
-							/>
-						}
-					/>
-					<Route path="/signup2" element={<Signup2 />} />
-					<Route
-						path="/chats"
-						element={
-							<PrivateRoute
-								element={<Chat />}
-								alternateElement={<Signin />}
-								isAuthenticated={isAuthenticated}
-							/>
-						}
-					/>
-					<Route path="/profile/:id" element={<Profile />} />
-					<Route path="*" element={<NotFound />} />
-				</Routes>
-			</Router>
-		</div>
+				</Helmet>
+				<Router>
+					{isAuthenticated ? <SignedInNavbar /> : <SignedOutNavbar />}
+					<Routes>
+						<Route
+							path="/"
+							element={
+								<PrivateRoute
+									element={<Home />}
+									alternateElement={<FrontPage />}
+									isAuthenticated={isAuthenticated}
+								/>
+							}
+						/>
+						<Route
+							path="/home"
+							element={
+								<PrivateRoute
+									element={<Home />}
+									alternateElement={<FrontPage />}
+									isAuthenticated={isAuthenticated}
+								/>
+							}
+						/>
+						<Route
+							path="/signin"
+							element={
+								<PrivateRoute
+									element={<Home />}
+									alternateElement={<Signin />}
+									isAuthenticated={isAuthenticated}
+								/>
+							}
+						/>
+						<Route
+							path="/signup"
+							element={
+								<PrivateRoute
+									element={<Home />}
+									alternateElement={<Signup />}
+									isAuthenticated={isAuthenticated}
+								/>
+							}
+						/>
+						<Route path="/signup2" element={<Signup2 />} />
+						<Route
+							path="/chats"
+							element={
+								<PrivateRoute
+									element={<Chat />}
+									alternateElement={<Signin />}
+									isAuthenticated={isAuthenticated}
+								/>
+							}
+						/>
+						<Route path="/profile/:id" element={<Profile />} />
+						<Route path="*" element={<NotFound />} />
+					</Routes>
+				</Router>
+			</div>
+		</HelmetProvider>
 	);
 }
 
--- findbuddy_ui/src/components/FrontPage.js
+++ findbuddy_ui/src/components/FrontPage.js
@@ -13,16 +13,15 @@ function FrontPage() {
 				}}
 			>
 				<h3 style={{ textAlign: "center", margin: "0 auto" }}>
-					The buddies platform - where interests become friendships{" "}
+					The buddies platform - where common interests become friendships{" "}
 				</h3>
 				<div style={{ margin: "1em 1em" }}>
-					You want to try a Muay Thai class, but you can't find friends to go
+					You want to try that Muay Thai class, but you can't find friends to go
 					with. You want to head to the club, but your clique is not available.
-					You want to travel to Thailand, but your travel buddies have clashing
-					schedules. FindBuddy is a platform where you can search and find
-					like-minded people with similar interests and values, or meet
-					different people altogether. If you want new buddies to do things
-					with, what are you waiting for?
+					You want to explore the outskirts of Thailand, but your travel buddy
+					has clashing schedules. FindBuddy is a platform where you can search
+					and find like-minded people with similar interests and values. If you
+					are looking for new buddies, what are you waiting for?
 				</div>
 				<img
 					style={{ objectFit: "cover", height: "300px", width: "440px" }}
