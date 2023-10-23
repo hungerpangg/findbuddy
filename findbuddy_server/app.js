8856195f964f5be0588d1fb07379ee903261b005
Ka Wai <pangkawai22@gmail.com>
2023-10-23 20:37:43 +0800

    Change cors url to new domain

--- findbuddy_server/app.js
+++ findbuddy_server/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: "https://findbuddy.onrender.com",
+		origin: "https://findbuddyhub.com",
 		methods: "GET,POST,PUT,DELETE", // The allowed HTTP methods
 		allowedHeaders: "Content-Type,Authorization", // The allowed request headers
 		credentials: true,
